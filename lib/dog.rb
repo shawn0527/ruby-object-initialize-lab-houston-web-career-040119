a81e826936e1e9c5510308c57dae4198ef8192ac
Shawn Ren <shawnren527@gmail.com>
2019-04-03 04:09:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 class Dog
   def initialize(dog_name, dog_breed)
     @name = dog_name
-    @breed = dog_breed
-  end
-
-  def initialize(dog_name)
-    @name = dog_name
-    @breed = "Mutt"
+    @breed ||= dog_breed
   end
 end
