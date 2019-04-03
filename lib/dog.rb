eefaf8142aec6eb167a2d00845ecbb06eecf2e68
Shawn Ren <shawnren527@gmail.com>
2019-04-03 04:01:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   def initialize(dog_name, dog_breed)
     @name = dog_name
-    if dog_breed.size > 0
+    if dog_breed.size == 0
       @breed = "Mutt"
     else
       @breed = dog_breed
